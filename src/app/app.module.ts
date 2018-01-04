c26b37d4444ffdda434bb2475073d6e64d5112bc
hamzaalaya <alaya.hamza@gmail.com>
2018-01-04 12:36:51 +0100

    fixed date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { DailyBasedExchangeRateComponent } from './daily-based-exchange-rate/daily-based-exchange-rate.component';
-import { ChartModule } from 'angular-highcharts';
+import {AppComponent} from './app.component';
+import {DailyBasedExchangeRateComponent} from './daily-based-exchange-rate/daily-based-exchange-rate.component';
+import {ChartModule} from 'angular-highcharts';
+import {NgDatepickerModule} from 'ng2-datepicker';
 
 
-import { CurrencyService } from './currency.service';
-
+import {CurrencyService} from './currency.service';
 
 @NgModule({
   declarations: [
@@ -23,9 +23,11 @@ import { CurrencyService } from './currency.service';
     HttpClientModule,
     FormsModule,
     HttpModule,
-    ChartModule
+    ChartModule,
+    NgDatepickerModule
   ],
   providers: [CurrencyService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/daily-based-exchange-rate/daily-based-exchange-rate.component.ts
+++ src/app/daily-based-exchange-rate/daily-based-exchange-rate.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CurrencyService } from '../currency.service';
-import { Chart } from 'angular-highcharts';
+import {Component, OnInit} from '@angular/core';
+import {CurrencyService} from '../currency.service';
+import {Chart} from 'angular-highcharts';
+import {DatepickerOptions} from 'ng2-datepicker';
+
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-daily-based-exchange-rate',
@@ -9,27 +13,48 @@ import { Chart } from 'angular-highcharts';
 })
 export class DailyBasedExchangeRateComponent implements OnInit {
 
-  constructor(private _currency: CurrencyService) { }
-  chart = {};
-  ngOnInit() {
-    this._currency.fetchExchangeRateForDateAndBase('2018-01-02', 'EUR')
-      .subscribe(res => {
-        let ratesKeysIterator = Object.keys(res.rates);
-        let data = [];
 
+  chart = {};
+  date = moment().format('YYYY-MM-DD');
+  exchangeRateForDateAndBase: any;
+  base = 'EUR';
+  otherCurrencies = [];
+  options: DatepickerOptions = {
+    minYear: 1970,
+    maxYear: 2030,
+    displayFormat: 'MMM D[,] YYYY',
+    barTitleFormat: 'MMMM YYYY',
+    firstCalendarDay: 0, // 0 - Sunday, 1 - Monday
+    // minDate: new Date(Date.now()), // Minimal selectable date
+    maxDate: new Date(Date.now())  // Maximal selectable date
+  };
+
+  constructor(private _currency: CurrencyService) {
+  }
+
+
+  fetchExchangeRateForDateAndBaseData() {
+
+    this._currency.fetchExchangeRateForDateAndBase(this.date, this.base)
+      .subscribe(res => {
+        this.exchangeRateForDateAndBase = res;
+        const ratesKeysIterator = Object.keys(this.exchangeRateForDateAndBase.rates);
+        const data = [];
         let index = 0;
+        this.otherCurrencies.push(this.base);
         ratesKeysIterator.forEach(element => {
-          let val = [];
-          val = [element, res.rates[element] / 10000];
+          let val: any[];
+          val = [element, this.exchangeRateForDateAndBase.rates[element] / 10000];
+          this.otherCurrencies.push(element);
           data.push(val);
           index++;
         });
         this.chart = new Chart({
           chart: {
-            type: 'column'
+            type: 'areaspline'
           },
           title: {
-            text: 'Exchange rate by date ' + res.base + ' : ' + res.date
+            text: 'Exchange rate by date ' + this.exchangeRateForDateAndBase.base + ' : ' + this.exchangeRateForDateAndBase.date
           },
           xAxis: {
             type: 'category',
@@ -48,8 +73,18 @@ export class DailyBasedExchangeRateComponent implements OnInit {
             name: '',
             data: data
           }]
-        })
-      })
+        });
+      });
   }
 
+  ngOnInit() {
+    this.fetchExchangeRateForDateAndBaseData();
+  }
+
+  update() {
+    this.date = moment(this.date).format('YYYY-MM-DD');
+    console.log(this.date);
+    console.log(this.base);
+    this.fetchExchangeRateForDateAndBaseData();
+  }
 }
